eb19e5cc2f20ba022f4d8215141a2746e9fd9a4c
steadxcz <vemcenko@gmail.com>
2022-03-31 19:11:09 +0300

    added database

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('alive');
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.PORT;
+const sqlite3 = require('sqlite3');
+
+
+
+const db = new sqlite3.Database('./database/database.db');
+
+app.get('/', (req, res) => {
+  res.json({message: 'alive'});
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+db.all("SELECT * FROM users", (error, rows) => {
+    rows.forEach((row) => {
+        console.log(row.id + " " + row.login);
+    })
+});
