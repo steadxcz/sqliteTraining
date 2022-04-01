4d56897ec00f9cef889353b32df79be42704f183
steadxcz <vemcenko@gmail.com>
2022-04-01 14:59:14 +0300

    added router and organized pages structure a bit

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
+
+const port = 80 || process.env.PORT;
+const sqlite3 = require('sqlite3');
+const querystring = require('querystring');
+var url = require('url');
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT;
-const sqlite3 = require('sqlite3');
-
-
-
+const path = require('path');
+const router = express.Router();
 const db = new sqlite3.Database('./database/database.db');
 
-app.get('/', (req, res) => {
-  res.json({message: 'alive'});
+
+/************************************************************/
+//app.use(express.static(path.join(__dirname, 'public')));
+app.listen(port);
+app.use('/', router);
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/root/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+router.get('/api', function (req, res) {
+  var reqUrl = url.parse(req.url, true);
+  res.end("APIRESPONSE");
+  if (reqUrl.query.a != undefined) {
+    console.log('got a request');
+  }
 });
+
+
 
 
 db.all("SELECT * FROM users", (error, rows) => {
-    rows.forEach((row) => {
-        console.log(row.id + " " + row.login);
-    })
+  rows.forEach((row) => {
+    console.log(row.id + " " + row.login);
+  })
 });
